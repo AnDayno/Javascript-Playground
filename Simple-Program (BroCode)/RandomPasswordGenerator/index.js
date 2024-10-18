36da2789c4b4a822dba403906b6587d33d7813aa
AnDayno <162073486+AnDayno@users.noreply.github.com>
2024-10-18 21:22:57 +0800

    JSPG: Created RandomPasswordGenerator
    This JavaScript code generates a random password based on user-specified criteria. It allows you to choose the password length and whether to include lowercase letters, uppercase letters, numbers, and symbols.

---
+++ Simple-Program (BroCode)/RandomPasswordGenerator/index.js
@@ -0,0 +1,49 @@
+function GeneratePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols)
+{
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVQXYZ";
+    const numberChars = "0123456789";
+    const symbolChars = "!@#$%^&*()_+,./";
+
+    let allowedChars = "";
+    let password = "";
+
+    allowedChars += includeLowercase ? lowercaseChars : "";
+    allowedChars += includeUppercase ? uppercaseChars : "";
+    allowedChars += includeNumbers ? numberChars : "";
+    allowedChars += includeSymbols ? symbolChars : "";
+
+    if(length <= 0)
+    {
+        return `(password length must be at least 1)`;
+    }
+    if(allowedChars.length === 0)
+    {
+        return `(At least 1 set of character needs to be selected)`;
+    }
+
+    for(let i = 0; i < length; i ++)
+    {
+        const randomIndex = Math.floor(Math.random() * allowedChars.length) + 1;
+        password += allowedChars[randomIndex];
+    }
+    
+    return password;
+}
+
+const passwordLenght = 12;
+const includeLowercase = true;
+const includeUppercase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+
+const password = GeneratePassword 
+(
+    passwordLenght, 
+    includeLowercase, 
+    includeUppercase, 
+    includeNumbers, 
+    includeSymbols
+);
+
+console.log(`Generated password: ${password}`);
