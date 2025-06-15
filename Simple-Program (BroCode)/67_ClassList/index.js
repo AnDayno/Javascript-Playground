1fe9041ad7fa309f65be88bfa0243ea592809926
An <162073486+AnDayno@users.noreply.github.com>
2025-06-15 17:15:57 +0800

    JSPG: Added ClassList Example 1

---
+++ Simple-Program (BroCode)/67_ClassList/index.js
@@ -0,0 +1,70 @@
+/*
+    classList = Element property in JavaScript used to interact 
+                with an element's list of classes (CSS classes)
+                Allows you to make reusable classes for many elements 
+                across your webpage.
+
+    add()
+    remove()
+    toggle(Remove if present, Add if not)
+    replace(oldClass, newClass)
+    contains()
+*/
+
+const myH1 = document.getElementById("myH1");
+const myButton = document.getElementById("myButton");
+
+// ----------------- ADD And Remove -----------------
+// myButton.classList.add("enabled");
+// myButton.classList.remove("enabled")
+
+// myButton.addEventListener("mouseover", e => {
+//     e.target.classList.add("hover");
+// });
+
+// myButton.addEventListener("mouseout", e => {
+//     e.target.classList.remove("hover");
+// }); 
+
+// ----------------- Toggle -----------------
+
+// myButton.addEventListener("mouseover", e => {
+//     e.target.classList.toggle("hover");
+// });
+
+// myButton.addEventListener("mouseout", e => {
+//     e.target.classList.toggle("hover");
+// });
+
+// ----------------- Replace -----------------
+
+// myButton.classList.add("enabled");
+
+// myButton.addEventListener("click", e => {
+//     e.target.classList.replace("enabled", "disabled");
+// });
+
+// ----------------- Contains -----------------
+myH1.classList.add("enabled");
+
+myH1.addEventListener("click", e => {
+    if(e.target.classList.contains("disabled")){
+        e.target.textContent += "😒";
+    }
+    else{
+        e.target.classList.replace("enabled","disabled");
+    }
+});
+
+myButton.classList.add("enabled");
+
+myButton.addEventListener("click", e => {
+
+    if(e.target.classList.contains("disabled")){
+        e.target.textContent = "It is already Disabled";
+    }
+    else{
+        e.target.classList.replace("enabled", "disabled");
+    }
+});
+
