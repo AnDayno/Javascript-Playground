f51ebdfc333add4ed28353d071440271d3640709
An <162073486+AnDayno@users.noreply.github.com>
2025-06-17 13:06:36 +0800

    JSPG: Added Promises

---
+++ Simple-Program (BroCode)/71_Promises/index.js
@@ -0,0 +1,66 @@
+/*
+    Promises = An object that manages asynchronous operations.
+               Wrap a Promise Object around {asynchronous code}
+               "I promise to return a value"
+               PENDING -> RESOLVED or REJECTED
+               new Promises((resove, reject) => {asynchronous code})
+
+    DO THESE CHORES IN ORDER
+
+    1. WALK THE DOG
+    2. CLEAN THE KITCHEN
+    3. TAKE OUT THE TRASH
+*/
+
+function walkDog(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+        const dogWalked = true;
+
+        if(dogWalked){
+            resolve("You walk the dog 🐕");
+        }
+        else{
+            reject("YOU LOST THE DOG!");
+        }
+    }, 1500);
+    });
+}
+
+function cleanKitchen(){
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+
+        const kitchenCleaned = false;
+
+        if(kitchenCleaned){
+            resolve("You clean the kitchen 🧹");
+        }
+        else{
+            reject("YOU BURNED DOWN THE KITCHEN");
+        }
+    }, 2500);
+    });
+}
+
+function takeOutTrash(){
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+
+        const trashTakenOut = true;
+
+        if(trashTakenOut){
+                    resolve("You take out the trash ♻️");
+        }
+        else{
+            reject("THE DOG ATE THE TRASH");
+        }
+    }, 500);
+    });
+}
+
+walkDog().then(value => {console.log(value); return cleanKitchen()})
+         .then(value => {console.log(value); return takeOutTrash()})
+         .then(value => {console.log(value); console.log("You Finished All The Task")})
+         .catch(error => console.error(error));
